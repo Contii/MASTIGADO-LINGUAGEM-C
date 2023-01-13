0be5c6f5cb24f2c9b672879a5f0eedd36a3d5811
Joao Conti <joaovitorconti@live.com>
2023-01-13 03:15:20 -0300

    Adicionado 01-Intro-Ponteiros

---
+++ 01-Intro-Ponteiros/structs.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+/* ------------------------------- */
+struct data{
+   int	dia;
+   int	mes;
+   int	ano;
+};
+/* ------------------------------- */
+struct pessoa{
+   int	Idade;
+   char	Nome[25];
+};
+/* ------------------------------- */
+#endif
